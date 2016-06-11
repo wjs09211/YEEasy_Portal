75c7249bddc080778b046e5688009b6deb7b908e
Sleep <wjs09211>
2016-06-11 17:58:35 +0800

    README

--- Info/Args.py
+++ Info/Args.py
@@ -1,19 +1,26 @@
 import argparse
 from oauth2client import tools
+import textwrap
 
-parser = argparse.ArgumentParser(parents=[tools.argparser],
-                                 description="YEEasy Portal linux command line operate yzu portal")
+parser = argparse.ArgumentParser(parents=[tools.argparser], formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description=textwrap.dedent('''\
+                                            YEEasy Portal
+                                 ----------------------------------------
+                                 linux command line operate yzu portal
+                                        more easy more powerful !!!
+                                 '''))
 
-# choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
-parser.add_argument('-l', '--login', action='store_true', help='login')
-parser.add_argument('-c', '--classs', action='store_true')
-parser.add_argument('-cs', '--class_schedule', action='store_true')
-parser.add_argument('-i', '--class_info', nargs="+")
-parser.add_argument('-t', '--teach_material', nargs="+")
-parser.add_argument('-hw', '--homework', nargs="+")
-parser.add_argument('-a', '--auto', type=int)
-parser.add_argument('-f', '--find', nargs=2)
-parser.add_argument('-avg', '--average', nargs='*')
-parser.add_argument('-g', '--grade', nargs='*')
-parser.add_argument('-goo', '--google_calendar', action='store_true')
+parser.add_argument('-l', '--login', action='store_true', help='login the yzu portal, need use this first')
+parser.add_argument('-c', '--classs', action='store_true', help='can look what class you study this semester')
+parser.add_argument('-cs', '--class_schedule', action='store_true', help='can look your school timetable')
+parser.add_argument('-i', '--class_info', nargs="+", help='can look your class info')
+parser.add_argument('-t', '--teach_material', nargs="+", help='can look and download your class teach material')
+parser.add_argument('-hw', '--homework', nargs="+", help='can look and download and upload your class homework')
+parser.add_argument('-a', '--auto', type=int, help='auto fill yzu question')
+parser.add_argument('-f', '--find', nargs=2,
+                    help='find keyword in all teach material, only support these file type: doc, docx, pptx, pdf, txt')
+parser.add_argument('-avg', '--average', nargs='*', help='show average grade, use with --grade')
+parser.add_argument('-g', '--grade', nargs='*', help='show grade up or down the number')
+parser.add_argument('-goo', '--google_calendar', action='store_true',
+                    help='import your school timetable to Google Calendar')
 args = parser.parse_args()
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # YEEasy Portal
 
-This is a linux command line tool. It can easily to operate [Yuan Ze University](https://portalx.yzu.edu.tw/PortalSocialVB/Login.aspx) portal. It is fast and have more powerful function
+This is a linux command line tool. It can easily to operate [Yuan Ze University](https://portalx.yzu.edu.tw/PortalSocialVB/Login.aspx) portal. It is fast and have more powerful function.
 
-## How to Install
+This project use Python 2.7 write.
+
+## How To Install?
 
 First, you need to clone this project
 
@@ -15,4 +17,27 @@ Then, you need to install something. I write a setup script. Execute it!
 cd YEEasy_Portal # need to move
 bash setup.sh
 alias yee="python Main.py" # can easy to use
+```
+
+## How To Use?
+
+*yee --login* can login the yzu portal
+```
+yee -l
+yee --login
+``` 
+*yee --class* can look what class you study this semester
+```
+yee -c
+yee --class
+```
+*yee -cs* can look your school timetable
+```
+yee -cs
+yee --class_schedule
+```
+*yee -i "classname" "number"* print class infomation.
+```
+yee -i CS312 # print all infomation
+yee -i CS312 5 # print only 5 infomation
 ```
