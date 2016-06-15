7555a51ff2cd987a2650ff3b0ef0fc052b7b7c45
Sleep <wjs09211>
2016-06-15 13:08:47 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install python-pip
-sudo apt-get install antiword
+sudo apt-get install python-pip -y
+sudo apt-get install antiword -y
 sudo pip install chardet
 sudo pip install python-pptx
 sudo pip install beautifulsoup4
 sudo pip install python-docx
 sudo pip install PyPDF2
 sudo pip install --upgrade google-api-python-client
+
+alias yee="python Main.py"
